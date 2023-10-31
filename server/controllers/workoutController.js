032a54ebab7d76ec2fbed905585f20caae52e398
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-10-31 16:48:08 -0500

    finished controllers

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require('../models/Workout')
+const mongoose = require('mongoose')
 
 //get all workouts
 const getWorkouts = async (req, res) => {
@@ -10,6 +11,10 @@ const getWorkouts = async (req, res) => {
 
 const getWorkout = async (req, res) => {
     const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "Cannot find workout"})
+    }
 
     const workout = await Workout.findById(id)
 
@@ -33,11 +38,52 @@ const createWorkout = async (req, res) => {
 
 
 //delete workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params; // Use "id" instead of "_id" to match the route parameter.
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "No workout found" });
+    }
+  
+    try {
+      const workout = await Workout.findOneAndDelete({ _id: id });
+  
+      if (!workout) {
+        return res.status(404).json({ error: "No workout found" });
+      }
+  
+      res.status(200).json(workout);
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" }); // Handle internal server errors gracefully.
+    }
+  }
 
 //update a workout
+
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "No workout found" })
+    }
+  
+    try {
+      const workout = await Workout.findOneAndUpdate({ _id: id }, req.body, { new: true })
+  
+      if (!workout) {
+        return res.status(404).json({ error: "No workout found" })
+      }
+  
+      res.status(200).json(workout);
+    } catch (err) {
+      res.status(500).json({ error: "Internal server error" })
+    }
+  }
 
 module.exports = {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    updateWorkout,
+    deleteWorkout
 }
--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {createWorkout, getWorkouts, getWorkout} = require('../controllers/workoutController')
+const {createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout} = require('../controllers/workoutController')
 
 router.get('/', getWorkouts)
 
@@ -8,12 +8,8 @@ router.get('/:id', getWorkout)
 
 router.post('/', createWorkout)
 
-router.delete('/:id', (req,res) => {
-    res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
-router.patch('/:id', (req,res) => {
-    res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
